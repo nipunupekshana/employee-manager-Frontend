b2d72f2ee7a68299562b2e2e9edae61f9994a2d8
Nipun Upekshana <nipun.upekshana@itelasoft.com>
2023-02-20 16:12:42 +0530

    Enhancement: added 404 page

--- pages/404.js
+++ pages/404.js
@@ -8,8 +8,8 @@ export default function NotFound() {
     <div className="d-flex flex-column justify-content-center align-items-center mt-5">
       <FaExclamationTriangle className="text-danger" size="5rem" />
       <h1>404</h1>
-      <p className="lead">Sorry, this page does not exists</p>
-      <Button className="btn btn-primary" onClick={() => router.push('/')}>
+      <p>Sorry, this page does not exists</p>
+      <Button className="btn btn-primary" onClick={() => router.push('/employee/list')}>
         Go Home
       </Button>
     </div>
